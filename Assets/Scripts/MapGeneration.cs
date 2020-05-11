2d4cdc80f0e2cfe0e5fc64426974e5907fce0e9e
Ignacio <Ignacio.f.l@hotmail.com>
2020-05-11 17:27:22 -0300

    Progress on map generation

---
+++ Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGeneration : MonoBehaviour
+{
+    public GameObject unbreakableWall;
+    public GameObject breakableWall;
+    [Space]
+    public int maxBlocksX;  
+    public int maxBlocksY;
+    [Space]
+    public int spaceBetweenXBlocks;
+    public int spaceBetweenYBlocks;
+
+    int obstacleCant = Random.Range(5, 15);
+    void Start()
+    {
+        for (int i = 0; i < maxBlocksY; i++)
+        {
+            for (int j = 0; j < maxBlocksX; j++)
+            {
+                Instantiate(unbreakableWall, new Vector3(i * spaceBetweenYBlocks, 0, j * spaceBetweenXBlocks), Quaternion.identity);
+
+                if(i % spaceBetweenYBlocks != 0 && j % spaceBetweenXBlocks != 0)
+                {
+                    Instantiate(breakableWall, new Vector3(i, 0, j), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
