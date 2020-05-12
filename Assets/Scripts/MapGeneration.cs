9a12ad713c66e7ff54e628cf26326b000fc41745
Ignacio <Ignacio.f.l@hotmail.com>
2020-05-11 23:03:41 -0300

    More progress on map generation

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -4,27 +4,70 @@ using UnityEngine;
 
 public class MapGeneration : MonoBehaviour
 {
+    public enum MapSize{
+        Small,
+        Medium,
+        Large
+    };
+
+    public MapSize mapSize = MapSize.Small;
+    //Will add player
+    [Space]
     public GameObject unbreakableWall;
     public GameObject breakableWall;
     [Space]
-    public int maxBlocksX;  
-    public int maxBlocksY;
-    [Space]
-    public int spaceBetweenXBlocks;
-    public int spaceBetweenYBlocks;
+    public GameObject smallMap;
+    public GameObject mediumMap;
+    public GameObject largeMap;
 
-    int obstacleCant = Random.Range(5, 15);
+    int spaceFix = 1;
+    int maxBlocksX;
+    int maxBlocksY;
+    int spaceBetweenXBlocks = 2;
+    int spaceBetweenYBlocks = 2;
+
+    int obstacleCant;
     void Start()
     {
+        switch (mapSize)
+        {
+            case MapSize.Small:
+                maxBlocksX = 5;
+                maxBlocksY = 9;
+                obstacleCant = Random.Range(5, 10);
+                Instantiate(smallMap);
+                break;
+
+            case MapSize.Medium:
+                maxBlocksX = 9;
+                maxBlocksY = 13;
+                obstacleCant = Random.Range(10, 15);
+                Instantiate(mediumMap);
+                break;
+
+            case MapSize.Large:
+                maxBlocksX = 13;
+                maxBlocksY = 17;
+                obstacleCant = Random.Range(12, 20);
+                Instantiate(largeMap);
+                break;
+
+            default:
+                break;
+        }
+
         for (int i = 0; i < maxBlocksY; i++)
         {
             for (int j = 0; j < maxBlocksX; j++)
             {
-                Instantiate(unbreakableWall, new Vector3(i * spaceBetweenYBlocks, 0, j * spaceBetweenXBlocks), Quaternion.identity);
+                Instantiate(unbreakableWall, new Vector3((i * spaceBetweenYBlocks) - ((maxBlocksY * spaceBetweenYBlocks) / 2) + spaceFix, 0, (j * spaceBetweenXBlocks) - ((maxBlocksX * spaceBetweenXBlocks) / 2) + spaceFix), Quaternion.identity);
 
                 if(i % spaceBetweenYBlocks != 0 && j % spaceBetweenXBlocks != 0)
                 {
-                    Instantiate(breakableWall, new Vector3(i, 0, j), Quaternion.identity);
+                    for (int k = 0; k < obstacleCant; k++)
+                    {
+                        Instantiate(breakableWall, new Vector3(i, 0, j), Quaternion.identity);
+                    }                   
                 }
             }
         }
