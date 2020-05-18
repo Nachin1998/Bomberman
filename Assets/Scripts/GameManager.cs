48367e5a1c245dab96d65d6f9748727ca3936b9a
Ignacio <Ignacio.f.l@hotmail.com>
2020-05-17 23:58:20 -0300

    Final fi

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviour
     {
         if (gameOver)
         {
-            SceneManager.LoadScene("Win");
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
